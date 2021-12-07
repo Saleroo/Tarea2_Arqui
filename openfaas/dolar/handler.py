7b49edfec4ed62b4732ac0fcdadabdcc575d5b88
Saleroo <50508769+Saleroo@users.noreply.github.com>
2021-12-07 00:51:39 -0300

    Add files via upload

---
+++ openfaas/dolar/handler.py
@@ -0,0 +1,18 @@
+import http.client
+
+conn = http.client.HTTPSConnection("mindicador.cl")
+payload = ''
+headers = {}
+conn.request("GET", "/api/dolar", payload, headers)
+res = conn.getresponse()
+data = res.read()
+print(data.decode("utf-8"))
+
+
+def handle(req):
+    """handle a request to the function
+    Args:
+        req (str): request body
+    """
+
+    return data.decode("utf-8")

0c5d38e6b6ff56666c2a7852e316b025b3ad6af5
Saleroo <50508769+Saleroo@users.noreply.github.com>
2021-12-07 00:52:15 -0300

    Add files via upload

---
+++ openfaas/crucigrama/handler.py
@@ -0,0 +1,7 @@
+def handle(req):
+    """handle a request to the function
+    Args:
+        req (str): request body
+    """
+    link="https://puzzel.org/es/features/crear-crucigramas"
+    return link

8082d3d801000d43f61fe4d00115b1a361c3e6fb
Diertotech <87912147+Diertotech@users.noreply.github.com>
2021-12-07 00:48:10 -0300

    Delete crucigrama directory

--- crucigrama/handler.py
+++
@@ -1,7 +0,0 @@
-def handle(req):
-    """handle a request to the function
-    Args:
-        req (str): request body
-    """
-    link="https://puzzel.org/es/features/crear-crucigramas"
-    return link
